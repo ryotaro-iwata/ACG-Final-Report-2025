e685bac30889fa6a5ad2cf3002cc8819b13b33d5
ryotaro-iwata <iwata.ryotarou@gmail.com>
2025-12-31 23:47:47 +0900

    Suspenseを追加

--- src/components/Scene/SceneObjectWrapper.tsx
+++ src/components/Scene/SceneObjectWrapper.tsx
@@ -1,6 +1,7 @@
 import { SceneObject } from "../Model/scene-object/index";
 import { testToonShader } from "../../shaders/test-toon/test-toon.tsx";
 import { verySimpleShader } from "../../shaders/very-simple/very-simple.tsx";
+import { Suspense } from "react";
 
 type SceneObjectWrapperProps = {
   lightX: number;
@@ -11,52 +12,61 @@ export const SceneObjectWrapper = ({ lightX, lightY, lightZ, }: SceneObjectWrapp
   return (
     <>
       {/* チョココロネ*/}
-      <SceneObject
-        modelPath="/models/choco_coromet/coromet.gltf"
-        shader={testToonShader}
-        uniforms={{
-          time: performance.now(),
-          colorTint: [1, 0.5, 0.5],
-          lightDirection: [lightX, lightY, lightZ],
-          lightIntensity: 1.0,
-        }}
-        object={{
-          position: [0, 0, 0],
-          rotation: [0, 0, 0],
-          scale: 1,
-        }}
-      />
+      <Suspense fallback={null}>
+        <SceneObject
+          modelPath="/models/choco_coromet/coromet.gltf"
+          shader={testToonShader}
+          uniforms={{
+            time: performance.now(),
+            colorTint: [1, 0.5, 0.5],
+            lightDirection: [lightX, lightY, lightZ],
+            lightIntensity: 1.0,
+          }}
+          object={{
+            position: [0, 0, 0],
+            rotation: [0, 0, 0],
+            scale: 1,
+          }}
+        />
+      </Suspense>
+
 
       {/*動物 */}
-      <SceneObject
-        modelPath="/models/horse.gltf"
-        shader={testToonShader}
-        uniforms={{
-          time: performance.now(),
-          colorTint: [1, 0.5, 0.5],
-          lightDirection: [lightX, lightY, lightZ],
-        }}
-        object={{
-          position: [0, 0, 0],
-          rotation: [0, 0, 0],
-          scale: 1,
-        }}
-      />
+      <Suspense fallback={null}>
+        <SceneObject
+          modelPath="/models/horse.gltf"
+          shader={testToonShader}
+          uniforms={{
+            time: performance.now(),
+            colorTint: [1, 0.5, 0.5],
+            lightDirection: [lightX, lightY, lightZ],
+          }}
+          object={{
+            position: [0, 0, 0],
+            rotation: [0, 0, 0],
+            scale: 1,
+          }}
+        />
+      </Suspense>
+
 
       {/* チョココロネ2*/}
-      <SceneObject
-        modelPath="/models/choco_coromet/coromet.gltf"
-        shader={verySimpleShader}
-        uniforms={{
-          lightDirection: [lightX, lightY, lightZ],
-          lightIntensity: 1.0,
-        }}
-        object={{
-          position: [0, 2.5, 0],
-          rotation: [0, 0, 0],
-          scale: 0.3,
-        }}
-      />
+      <Suspense fallback={null}>
+        <SceneObject
+          modelPath="/models/choco_coromet/coromet.gltf"
+          shader={verySimpleShader}
+          uniforms={{
+            lightDirection: [lightX, lightY, lightZ],
+            lightIntensity: 1.0,
+          }}
+          object={{
+            position: [0, 2.5, 0],
+            rotation: [0, 0, 0],
+            scale: 0.3,
+          }}
+        />
+      </Suspense>
+
     </>
   );
 };
