d31aa2696a26080639ab9abf805f66d1fd7c0a45
ryotaro-iwata <iwata.ryotarou@gmail.com>
2026-01-22 23:15:04 +0900

    useHandWriteを追加

--- src/components/Scene/SceneObjectWrapper.tsx
+++ src/components/Scene/SceneObjectWrapper.tsx
@@ -2,22 +2,27 @@ import { SceneObject } from "../Model/scene-object/index";
 import { Suspense } from "react";
 import { useNormalRules } from "./_hooks/useNormalRules";
 import { useVaporRules } from "./_hooks/useVaporRules";
+import { useHandWriteRules } from "./_hooks/useHandWriteRules";
 import { useControls } from "leva";
 
 export const SceneObjectWrapper = () => {
   const { themeMode } = useControls("SHader Theme", {
     themeMode: {
       value: "Normal",
-      options: ["Normal", "Vaporwave"]
+      options: ["Normal", "Vaporwave", "Handwrite"]
     }
   })
   const normalRules = useNormalRules();
   const vaporRules = useVaporRules();
+  const handWriteRules = useHandWriteRules();
 
   let rules;
   switch (themeMode) {
     case "Vaporwave":
       rules = vaporRules;
+      break;
+    case "Handwrite":
+      rules = handWriteRules;
       break;
     case "Normal":
     default:
